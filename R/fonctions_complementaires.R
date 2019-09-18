1b7ce7ea02f33cb9042c1c3fa6c0fed8fd5232c0
Rémi Flicoteaux <remi.flicoteaux@aphp.fr>
2019-09-18 16:52:29 +0200

    maj

--- R/fonctions_complementaires.R
+++ R/fonctions_complementaires.R
@@ -407,16 +407,3 @@ attribution_statut_nx_patient<-function(df){
   
   return(df)
 }
-
-#' Pipe
-#'
-#' Put description here
-#'
-#' @importFrom magrittr %>%
-#' @name %>%
-#' @rdname pipe
-#' @export
-#' @param lhs,rhs specify what lhs and rhs are
-#' @examples
-#' # some examples if you want to highlight the usage in the package
-NULL
